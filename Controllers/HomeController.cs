9c7d7beec3ed0bb75dea6256ff36d2c11c251339
昱騏 胡 <hutoby7@gmail.com>
2022-10-05 19:16:53 +0800

    1005 作業都 放在HomeWoekController裡面

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -31,14 +31,24 @@ namespace slnAJAX.Controllers
         }
         public IActionResult Test()
         {
-          
             return View();
         }
-
+        public IActionResult Fetch()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Address()
+        {
+            return View();
         }
     }
 }
